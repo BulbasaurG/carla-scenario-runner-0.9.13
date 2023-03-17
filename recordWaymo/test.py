2f9593f4a387843760afee5d1dad3270459df1f5
BulbasaurG <detian.guo@outlook.com>
2023-03-17 17:36:15 +0100

    feat: different trying

--- recordWaymo/test.py
+++ recordWaymo/test.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 FILE_PATH = Path(__file__).parent
 
-with open(str(FILE_PATH/"BikePassingby_0.pkl"), 'rb') as f:
+with open(str(FILE_PATH/"SignalizedJunctionLeftTurn-00000-of-00010.pkl"), 'rb') as f:
     data = pickle.load(f)
 
 for key in data.keys():
@@ -13,3 +13,28 @@ for key in data.keys():
     print("\tshape: ", data[key].shape)
     print("\tdtype: ", data[key].dtype)
 # %%
+import matplotlib.pyplot as plt
+cw_ind = np.where(data['roadgraph_samples/type']==18)[0]
+plt.scatter(data["roadgraph_samples/xyz"][cw_ind,0], data["roadgraph_samples/xyz"][cw_ind,1],s=1)
+plt.show()
+
+# %%s
+
+car_ind = np.where(data['state/type']==1)[0]
+ped_ind = np.where(data['state/type']==2)[0]
+cyc_ind = np.where(data['state/type']==3)[0]
+
+for i, car in enumerate(car_ind):
+    plt.plot(data["state/x"][car].squeeze(),data["state/y"][car].squeeze(),label=f"car_{i}")
+for i, ped in enumerate(ped_ind):
+    plt.plot(data["state/x"][ped].squeeze(),data["state/y"][ped].squeeze(),label=f"ped_{i}")
+plt.legend()
+plt.show()
+
+# %%
+# PedestrianCrossing_0 not moved, actor state shape (128,1201)
+# bikepassingby not moved, actor state shape (128,1201)
+# SignalizedJunctionLeftTurn_1 moved, actor state shape (128,318)
+print(data["state/x"][ped_ind].squeeze())
+
+
